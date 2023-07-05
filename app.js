625e980ebfa7821e85f5a5b9b1d3360526c4d235
Cagatay Karaduman <karadumancagatay@gmail.com>
2023-07-05 11:29:25 +0300

    partials created

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
 const express = require('express');
-
+const path = require("path");
 const app = express();
+const ejs = require("ejs")
+
+app.set("view engine", "ejs");
+
+app.use(express.static("public"));
 
 app.get('/', (req, res) => {
-
-  const blog = {
-    id: 1,
-    title: "Blog Title",
-    description: "Blog description"
-  }
-  res.send(blog)
+  res.render("index");
+})
+app.get('/about', (req, res) => {
+  res.render("about");
+})
+app.get('/add_post', (req, res) => {
+  res.render("add_post");
 })
 
 const port = 3000;
