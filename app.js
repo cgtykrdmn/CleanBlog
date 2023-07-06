ac04610f228f3339e60ef4d7abe44fd1c4ccfe4a
Cagatay Karaduman <karadumancagatay@gmail.com>
2023-07-06 12:08:11 +0300

    database created

--- app.js
+++ app.js
@@ -1,23 +1,46 @@
-const express = require('express');
+const express = require("express");
+const mongoose = require("mongoose");
+const ejs = require("ejs");
 const path = require("path");
+const exp = require("constants");
 const app = express();
-const ejs = require("ejs")
+const Post = require("./models/Post");
+
+//Connect DB
+mongoose.connect("mongodb://localhost/cleanblog-test-db", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 app.set("view engine", "ejs");
 
+//MIDDLEWARES
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.render("index");
-})
-app.get('/about', (req, res) => {
+app.get("/", async (req, res) => {
+  const posts = await Post.find({});
+  res.render("index", {
+    posts
+  });
+});
+
+
+app.get("/about", (req, res) => {
   res.render("about");
-})
-app.get('/add_post', (req, res) => {
+});
+app.get("/add_post", (req, res) => {
   res.render("add_post");
-})
+});
 
-const port = 3000;
+app.post("/posts", async (req, res) => {
+  await Post.create(req.body);
+  res.redirect("/");
+});
+
+
+const port = 4000;
 app.listen(port, () => {
   console.log(`${port} has been started.`);
 });
