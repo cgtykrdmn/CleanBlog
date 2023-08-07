58bc2c8aee45102f4ed2ded3bad07d67c6339334
Cagatay Karaduman <karadumancagatay@gmail.com>
2023-08-07 09:49:58 +0300

    post update and delete

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const path = require("path");
 const exp = require("constants");
 const app = express();
 const Post = require("./models/Post");
+const fileUpload = require("express-fileupload");
+const methodOverride = require('method-override');
+
 
 //Connect DB
 mongoose.connect("mongodb://localhost/cleanblog-test-db", {
@@ -18,21 +21,26 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(fileUpload());
+app.use(
+  methodOverride("_method", {
+    methods: ["POST", "GET"],
+  })
+);
 
 app.get("/", async (req, res) => {
   const posts = await Post.find({});
   res.render("index", {
-    posts
+    posts,
   });
 });
 
 app.get("/posts/:id", async (req, res) => {
   const post = await Post.findById(req.params.id);
   res.render("post", {
-    post
+    post,
   });
 });
-
 
 app.get("/about", (req, res) => {
   res.render("about");
@@ -46,6 +54,26 @@ app.post("/posts", async (req, res) => {
   res.redirect("/");
 });
 
+app.get("/posts/edit/:id", async (req, res) => {
+  const post = await Post.findOne({ _id: req.params.id });
+  res.render("edit", {
+    post,
+  });
+});
+
+app.put("/posts/:id", async (req, res) => {
+  const post = await Post.findOne({ _id: req.params.id });
+  post.title = req.body.title;
+  post.description = req.body.description;
+  post.save();
+
+  res.redirect(`/posts/${req.params.id}`);
+});
+
+app.delete('/posts/:id', async (req, res) => {
+  await Post.findByIdAndRemove(req.params.id);
+  res.redirect('/');
+});
 
 const port = 4000;
 app.listen(port, () => {
